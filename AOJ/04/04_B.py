b8d83581b82b6e31d2784789c340d4ce61cf61f9
Kentaro Hirasawa <kentaro99114@gmail.com>
2022-11-01 10:22:13 +0900

    add AOJ/04

---
+++ AOJ/04/04_B.py
@@ -0,0 +1,4 @@
+r = float(input())
+area = r * r * 3.141592653589
+circle = 2 * 3.141592653589 * r
+print(area, circle)
---
+++ AOJ/04/04_D.py
@@ -0,0 +1,7 @@
+a = int(input())
+lst = list(map(int,input().split()))
+max = max(lst)
+min = min(lst)
+sum = sum(lst)
+print(f"{min} {max} {sum}")
+
